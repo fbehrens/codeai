7ad9b6da0cb5bc73c0af45de7d87024df3161807
Frank Behrens <fbehrens@gmail.com>
2023-09-13 14:15:45 +0200

    Fbutil.parse

--- src/lib/fbutil.ts
+++ src/lib/fbutil.ts
@@ -2,13 +2,14 @@ import OpenAI from 'openai';
 const openai = new OpenAI({
     // apiKey: 'my api key', // defaults to process.env["OPENAI_API_KEY"]
 });
+type Message = {
+    role: string;
+    content: string;
+};
 export default class Fbutil{
     static inc(n: number) {
         return n + 1;
     };
-    static dec(n: number) {
-        return n - 1;
-    }
     static async chat(content:string): Promise<string|null> {
         const params: OpenAI.Chat.ChatCompletionCreateParams = {
           messages: [{ role: 'user', content: content }],
@@ -16,5 +17,14 @@ export default class Fbutil{
         };
         const completion: OpenAI.Chat.ChatCompletion = await openai.chat.completions.create(params);
         return completion.choices[0].message.content;
+    };
+    static parse(dialog:string): Message[] {
+        const result:Message[] = [];
+        const paragraphs = dialog.split(/\n(?=[A-Z]+:)/);
+        for (const paragraph of paragraphs) {
+            const [role, content] = paragraph.split(':').map((item) => item.trim());
+            result.push({ role, content });
+        };
+        return result;
     }
 }
--- src/libtest/fbutil.test.ts
+++ src/libtest/fbutil.test.ts
@@ -2,12 +2,23 @@ import * as assert from 'assert';
 import Fbutil from '../lib/fbutil';
 
 describe('Fbutil', () => {
+
   it('inc', () => {
     let r = Fbutil.inc(6);
     assert.strictEqual(r,7);
   });
-  it('dec', () => {
-    let r = Fbutil.dec(6);
-    assert.strictEqual(r,5);
+
+  it('parse', ()=>{
+    const dialog = `SYSTEM: Lorem Ipsum bla
+USER: Hello Hello,
+I am here.
+ASSISTANT:  How are you?
+USER: I am`;
+    assert.deepEqual(Fbutil.parse(dialog),[
+        { role: 'SYSTEM', content: 'Lorem Ipsum bla' },
+        { role: 'USER', content: 'Hello Hello,\nI am here.' },
+        { role: 'ASSISTANT', content: 'How are you?' },
+        { role: 'USER', content: 'I am' }
+    ]);
   });
 });
