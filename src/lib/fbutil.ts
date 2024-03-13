d21bcaf4c6e964a3be71b98c8b689f4a89969c64
Frank Behrens <fbehrens@gmail.com>
2024-03-13 10:31:39 +0100

    fix typescript error

--- src/lib/fbutil.ts
+++ src/lib/fbutil.ts
@@ -1,12 +1,9 @@
 import OpenAI from 'openai';
+import { ChatCompletionMessageParam } from 'openai/resources';
 const openai = new OpenAI({
   // apiKey: 'my api key', // defaults to process.env["OPENAI_API_KEY"]
 });
 type Role = 'function' | 'system' | 'user' | 'assistant';
-type Message = {
-  role: Role;
-  content: string;
-};
 export default class Fbutil {
   static inc(n: number) {
     return n + 1;
@@ -36,15 +33,15 @@ export default class Fbutil {
       }
     }
   }
-  static parse(dialog: string): Message[] {
-    const result: Message[] = [];
+  static parse(dialog: string): ChatCompletionMessageParam[] {
+    const result: ChatCompletionMessageParam[] = [];
     const paragraphs = dialog.split(/\n(?=function:|user:|system:|assistant:)/);
     for (const paragraph of paragraphs) {
       const colon = paragraph.indexOf(':');
       const r = paragraph.slice(0, colon);
       const content = paragraph.slice(colon + 1).trim();
       const role = r as Role;
-      result.push({ role, content });
+      result.push({ role, content} as ChatCompletionMessageParam);
     }
     return result;
   }
