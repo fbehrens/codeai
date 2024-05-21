79eb72caac09f1fee81eb06a099e929b78ff4d49
Frank Behrens <fbehrens@gmail.com>
2024-05-21 12:41:48 +0200

    format on Save

--- src/libtest/fbutil.test.ts
+++ src/libtest/fbutil.test.ts
@@ -1,11 +1,11 @@
 import * as assert from 'assert';
-import Fbutil from '../lib/fbutil';
 import { ChatCompletionContentPartImage } from 'openai/resources/chat/completions';
+import Fbutil from '../lib/fbutil';
 
 const detail = 'low';
 const dir = '/Users/fb/Documents/Github/codeai/examples';
 describe('Fbutil', () => {
-  it('parse', async function() {
+  it('parse', async function () {
     const dialog = `## ignore
 this
 # system: ignore this sytem message
@@ -14,41 +14,56 @@ this
 I am here.
 assistant:  How are you?
 user: I am`;
-    const result = await Fbutil.parse(dialog,detail,dir,false);
+    const result = await Fbutil.parse(dialog, detail, dir, false);
     assert.deepEqual(result, [
       { role: 'system', content: 'Lorem: Ipsum bla' },
       { role: 'user', content: 'Hello Hello,\nI am here.' },
       { role: 'assistant', content: 'How are you?' },
       { role: 'user', content: 'I am' },
     ]);
-    const resultOnly = await Fbutil.parse(dialog,detail,dir,true);
+    const resultOnly = await Fbutil.parse(dialog, detail, dir, true);
     assert.deepEqual(resultOnly, [
       { role: 'system', content: 'Lorem: Ipsum bla' },
       { role: 'user', content: 'I am' },
     ]);
   });
-  describe('Image',async function (){
-      it('http', async function (){
-          const result = await Fbutil.parse(`user: Hello Hello![](http://image)`, detail, dir,false);
-          assert.deepEqual(result, [
-              { role: 'user', content: [
-                  { type: 'text',
-                    text: 'Hello Hello'},
-                  { type: 'image_url',
-                    // eslint-disable-next-line @typescript-eslint/naming-convention
-                    image_url: {
-                      url: 'http://image',
-                      detail } } ]
-                },
-            ]);
-        });
-      it('local base64', async function (){
-          const result = await Fbutil.parse(`user: Hello Hello![](fbehrens.jpeg)`, detail, dir,false);
-          const image  = result[0].content![1] as ChatCompletionContentPartImage;
-          const base64 = image.image_url.url;
-          assert.match(base64,/^data:image\/jpeg;base64,/);
-          assert.equal(base64.length,1883);
-        });
+  describe('Image', async function () {
+    it('http', async function () {
+      const result = await Fbutil.parse(
+        `user: Hello Hello![](http://image)`,
+        detail,
+        dir,
+        false
+      );
+      assert.deepEqual(result, [
+        {
+          role: 'user',
+          content: [
+            { type: 'text', text: 'Hello Hello' },
+            {
+              type: 'image_url',
+              // eslint-disable-next-line @typescript-eslint/naming-convention
+              image_url: {
+                url: 'http://image',
+                detail,
+              },
+            },
+          ],
+        },
+      ]);
     });
+    it('local base64', async function () {
+      const result = await Fbutil.parse(
+        `user: Hello Hello![](fbehrens.jpeg)`,
+        detail,
+        dir,
+        false
+      );
+      const image = result[0].content![1] as ChatCompletionContentPartImage;
+      const base64 = image.image_url.url;
+      assert.match(base64, /^data:image\/jpeg;base64,/);
+      assert.equal(base64.length, 1883);
+    });
+  });
 });
 console.log(new Date().toLocaleTimeString());
